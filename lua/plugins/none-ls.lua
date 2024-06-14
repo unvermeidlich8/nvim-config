3e99512375805eaf2106532b590fc1e702cba22d
unvermeidlich8 <romkapevchiy@gmail.com>
2024-06-14 23:29:32 +0300

    Init

---
+++ lua/plugins/none-ls.lua
@@ -0,0 +1,15 @@
+return {
+  "nvimtools/none-ls.nvim",
+  config = function()
+    local null_ls = require("null-ls")
+    null_ls.setup({
+      source = {
+        null_ls.builtins.formatting.stylua,
+        null_ls.builtins.formatting.gofumpt,
+        null_ls.builtins.formatting.goimports_reviser,
+        null_ls.builtins.formatting.golines,
+      },
+    })
+    vim.keymap.set('n','<leader>gf',vim.lsp.buf.format,{})
+  end
+}
---
+++ lua/vim-options.lua
@@ -0,0 +1,11 @@
+vim.cmd("set tabstop=2")
+vim.cmd("set expandtab")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.cmd("set number")
+
+vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
+vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
+vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
+vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
